d6daeb59bff2458c99ef6d464f3961843aa65698
Flamiiinius <rodrigommfr@gmail.com>
2022-12-04 17:35:45 +0000

    Day 4 small refactor

--- src/main/kotlin/com/flaminius/advent2022/Day4.kt
+++ src/main/kotlin/com/flaminius/advent2022/Day4.kt
@@ -7,30 +7,21 @@ fun main() {
 }
 
 class EWork(input: String) {
-    private var start: Int
-    private var end: Int 
-
-    init {
-        val values = input.split("-")
-        start = values[0].toInt()
-        end = values[1].toInt()
-    }
+    private val range: List<Int> = input.split("-").map(String::toInt)
 
     fun contains(other: EWork) =
-        (other.start <= start && end <= other.end)
+        (other.range[0] <= range[0] && range[1] <= other.range[1])
 
-    fun overlaps(other: EWork) = other.start in start..end
+    fun overlaps(other: EWork) = other.range[0] in range[0]..range[1]
 }
 
 fun List<EWork>.advContains() = this[0].contains(this[1]) || this[1].contains(this[0])
 fun List<EWork>.advOverlaps() = this[0].overlaps(this[1]) || this[1].overlaps(this[0])
-fun Boolean.toInt() = if (this) 1 else 0
 
 class Day04(private val input: List<String>) {
+    private fun getEWork(str: String) = str.split(",").map { EWork(it) }
 
-    private fun getEworks(str: String) = str.split(",").map { EWork(it) }
+    fun solve1() = input.count { line -> getEWork(line).advContains() }
 
-    fun solve1() = input.sumOf { line -> getEworks(line).advContains().toInt() }
-
-    fun solve2() = input.sumOf { line -> getEworks(line).advOverlaps().toInt() }
+    fun solve2() = input.count { line -> getEWork(line).advOverlaps() }
 }
